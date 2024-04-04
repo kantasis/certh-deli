56538799ef37fee5b38aefa65c24246854fa6de2
George Kantasis <kantasisg@gmail.com>
2024-04-04 12:03:25 +0300

    store the visual

---
+++ services/grafana/visuals/test_visual/main.js
@@ -0,0 +1,48 @@
+
+let values_fLst = [];
+let countries_sLst = [];
+
+data.series.map((s) => {
+
+   values_fLst = s.fields.find((f) =>
+      f.name === 'values_f'
+   ).values;
+
+   countries_sLst = s.fields.find((f) =>
+      f.name === 'country_str'
+   ).values;
+
+});
+
+indicator_Lst = []
+for (let i in countries_sLst) {
+   indicator_Lst[i] = {
+      name: countries_sLst[i],
+      max: 30,
+   }
+}
+
+return {
+   title: {
+      text: 'Basic Radar Chart'
+   },
+   // legend: {
+   //    data: ['Allocated Budget', 'Actual Spending']
+   // },
+   radar: {
+      // shape: 'circle',
+      indicator: indicator_Lst
+   },
+   series: [
+      {
+         name: 'Budget vs spending',
+         type: 'radar',
+         data: [
+            {
+               value: values_fLst
+               // name: 'Allocated Budget'
+            }
+         ]
+      }
+   ]
+};
