a9616c9571d26b79ddc15541e74126f363ca9cd2
George Kantasis <kantasisg@gmail.com>
2024-04-05 09:40:00 +0300

    update the homepage

--- services/grafana/visuals/test_visual/main.js
+++ services/grafana/visuals/test_visual/main.js
@@ -4,45 +4,52 @@ let countries_sLst = [];
 
 data.series.map((s) => {
 
-   values_fLst = s.fields.find((f) =>
-      f.name === 'values_f'
-   ).values;
+  if (s.refId != 'main_query')
+    return;
 
-   countries_sLst = s.fields.find((f) =>
-      f.name === 'country_str'
-   ).values;
+  values_fLst = s.fields.find((f) =>
+    f.name === 'values_f'
+  ).values;
+
+  countries_sLst = s.fields.find((f) =>
+    f.name === 'country_str'
+  ).values;
 
 });
 
 indicator_Lst = []
 for (let i in countries_sLst) {
-   indicator_Lst[i] = {
-      name: countries_sLst[i],
-      max: 30,
-   }
+  indicator_Lst[i] = {
+    name: countries_sLst[i],
+    max: 30,
+  }
+
 }
 
 return {
-   title: {
-      text: 'Basic Radar Chart'
+  title: {
+    text: 'Basic Radar Chart'
+  },
+  tooltip: {
+    trigger: 'item'
    },
    // legend: {
    //    data: ['Allocated Budget', 'Actual Spending']
    // },
-   radar: {
-      // shape: 'circle',
-      indicator: indicator_Lst
-   },
-   series: [
-      {
-         name: 'Budget vs spending',
-         type: 'radar',
-         data: [
-            {
-               value: values_fLst
-               // name: 'Allocated Budget'
-            }
-         ]
-      }
-   ]
+  radar: {
+    // shape: 'circle',
+    indicator: indicator_Lst
+  },
+  series: [
+    {
+      name: 'Budget vs spending',
+      type: 'radar',
+      data: [
+        {
+          value: values_fLst
+          // name: 'Allocated Budget'
+        }
+      ]
+    }
+  ]
 };
