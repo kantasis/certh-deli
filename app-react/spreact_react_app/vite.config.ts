4fa1c5d82172f70c78e92c23fe407abc3d94d32a
George Kantasis <kantasisg@gmail.com>
2024-06-06 15:10:57 +0300

    WIP on filters and interactivity

--- app-react/spreact_react_app/vite.config.ts
+++ app-react/spreact_react_app/vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // TODO: Check this one out
+  // server: { port: 3000, },
 })
