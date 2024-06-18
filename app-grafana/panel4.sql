e5c6c77b1571665ab5d89dcbb273fac60abee3e3
George Kantasis <kantasisg@gmail.com>
2024-06-18 16:50:16 +0300

    Use a full query instead of layering a view

--- app-grafana/panel4.sql
+++ app-grafana/panel4.sql
@@ -1,13 +1,19 @@
+CREATE EXTENSION IF NOT EXISTS tablefunc;
 
 SELECT 
    *
 FROM crosstab('
    SELECT 
-      year_str, 
-      country_str, 
-      values_f 
-   FROM prepivot_view 
-   WHERE year_str BETWEEN ${minyear_filter} AND ${maxyear_filter}
+      "Year" as year_str,
+      "Country" as country_str,
+      AVG("CRC_incidence_val_Percent") as values_f
+   FROM data_tbl 
+   WHERE true
+      AND age=''Age-standardized''
+      AND "Country" IN (''Greece'', ''Romania'', ''Lithuania'', ''Belgium'', ''Italy'', ''Spain'')
+   GROUP BY
+      "Country",
+      "Year"
    ORDER BY 1, 2
 ') AS ct (
    "Year" INT,
@@ -18,6 +24,7 @@ FROM crosstab('
    "Italy" DOUBLE PRECISION,
    "Spain" DOUBLE PRECISION
 )
-
 ORDER BY ct."Year"
 ;
+
+
