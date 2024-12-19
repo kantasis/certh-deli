fe1288fa02d2c799f05fedfd0789791134c1de9a
George Kantasis <kantasisg@gmail.com>
2024-12-19 10:51:15 +0200

    Add carousel and homepage video

--- app-react/spreact_react_app/src/components/Home.tsx
+++ app-react/spreact_react_app/src/components/Home.tsx
@@ -1,5 +1,24 @@
 
+import { Button, Dropdown } from 'react-bootstrap'; 
+import Carousel from 'react-bootstrap/Carousel';
+import ExampleCarouselImage from 'components/ExampleCarouselImage';
+
 const Home: React.FC = () => {
+
+   const style = {
+      image:{
+         width: '100%',
+         height: '100%',
+         objectFit: 'cover', // Maintains aspect ratio, while covering the area
+         padding: '2% 0%'
+      }
+   };
+
+   const carouselImages_strLst = [
+      "incidence.png",
+      "lifestyle.png",
+      "nutrition.png",
+   ];
 
    return (
       <div className="container">
@@ -7,6 +26,8 @@ const Home: React.FC = () => {
             <div className="hero-content">
                <h3>Welcome to the ONCODIR Policy Analytics Dashboard (DELI)</h3>
                <p>A web-based intelligence tool that integrates and visualizes data for reporting purposes.</p>
+               <br/>
+
                <iframe 
                   width="560" 
                   height="315" 
@@ -19,21 +40,22 @@ const Home: React.FC = () => {
                >
 
                </iframe>
-
-               <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
-                  <div className="carousel-inner">
-                     <div className="carousel-item active">
-                        <img className="d-block w-100" src="..." alt="First slide"/>
-                     </div>
-                     <div className="carousel-item">
-                        <img className="d-block w-100" src="..." alt="Second slide"/>
-                     </div>
-                     <div className="carousel-item">
-                        <img className="d-block w-100" src="..." alt="Third slide"/>
-                     </div>
-                  </div>
-               </div>
-
+               <Carousel>
+                  {carouselImages_strLst.map((carouselImage_str, index_i)=>(
+                     <Carousel.Item>
+                        <img 
+                           key={`image-${index_i}`} 
+                           className="d-block w-100"
+                           src={carouselImage_str}
+                           alt={`Slide #${index_i}`}
+                        />
+                        {/* <Carousel.Caption>
+                           <h3>First slide label</h3>
+                           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption> */}
+                     </Carousel.Item>
+                  ))}
+               </Carousel>
 
             </div>
          </header>
