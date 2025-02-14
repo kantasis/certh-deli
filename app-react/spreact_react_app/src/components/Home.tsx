c98b3555d011469e2139fc9a4cc97f448bbd28b1
Leonidas Kalpakis <leokalpakis@iti.gr>
2025-02-14 16:54:51 +0200

    New video

--- app-react/spreact_react_app/src/components/Home.tsx
+++ app-react/spreact_react_app/src/components/Home.tsx
@@ -1,12 +1,12 @@
 
-import { Button, Dropdown } from 'react-bootstrap'; 
+import { Button, Dropdown } from 'react-bootstrap';
 import Carousel from 'react-bootstrap/Carousel';
 import ExampleCarouselImage from 'components/ExampleCarouselImage';
 
 const Home: React.FC = () => {
 
    const style = {
-      image:{
+      image: {
          width: '100%',
          height: '100%',
          objectFit: 'cover', // Maintains aspect ratio, while covering the area
@@ -24,23 +24,28 @@ const Home: React.FC = () => {
       <div className="container">
          <header className="jumbotron">
             <div className="hero-content">
-               <br/>
+               <br />
                <h3>Welcome to the ONCODIR Policy Analytics Dashboard (DELI)</h3>
                <p>Α web-based intelligent dashboard with descriptive and predictive analytics to inform evidence-based policy decisions on CRC prevention.</p>
-               <br/>
+               <br />
 
-               <iframe 
-                  width="560" 
-                  height="315" 
-                  src="https://www.youtube.com/embed/nQolg4hVvBE?si=YcWJ5Tv2cniNZRYM" 
-                  title="YouTube video player" 
-                  frameborder="0" 
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                  referrerpolicy="strict-origin-when-cross-origin" 
+               {/* <iframe
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/nQolg4hVvBE?si=YcWJ5Tv2cniNZRYM"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerpolicy="strict-origin-when-cross-origin"
                   allowfullscreen
                >
 
-               </iframe>
+               </iframe> */}
+               <video width="560" height="315" controls poster="/src/assets/Oncodir photo for video.png">
+                  <source src="/src/assets/Oncodir with voiceover - no music.mp4" type="video/mp4"/>
+                     Your browser does not support the video tag.
+               </video>
+
                {/* <Carousel>
                   {carouselImages_strLst.map((carouselImage_str, index_i)=>(
                      <Carousel.Item>
